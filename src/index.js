8a3a2f049ccc68603b6cb189ca85aa681ef8f468
Alex Sagel <alex.sagel7587@gmail.com>
2017-12-24 18:51:31 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -8,9 +8,24 @@ const BUTCHER_PRODUCTS = [
 ];
 
 // Define these exported classes
-export class OlderCoaster extends React.Component {};
-export class InFrontOfYou extends React.Component {};
-export class ButcherShop extends React.Component {};
+export class OlderCoaster extends React.Component {
+  render(){
+    return React.createElement('div', {className: 'oldercoaster'},
+      React.createElement('p', {}, 'Passengers:'),
+      React.createElement('ul', {}, [
+          React.createElement('li', {}, 'Agnes'),
+          React.createElement('li', {}, 'Muriel'),
+      ])
+    )
+  }
+};
+
+export class InFrontOfYou extends React.Component {
+
+};
+export class ButcherShop extends React.Component {
+
+};
 
 ReactDOM.render(
   React.createElement('div', {}, [
